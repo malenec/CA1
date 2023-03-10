c54fe8a2ed7eb72782d57045e3e1dd87b06e2a8c
Mark Pedersen <sutsko@gmail.com>
2023-03-10 10:09:09 +0100

    setup unit tests

---
+++ src/test/java/facades/HobbyFacadeTest.java
@@ -0,0 +1,34 @@
+package facades;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class HobbyFacadeTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void getAllHobbies() {
+    }
+
+    @Test
+    void getHobbyById() {
+    }
+
+    @Test
+    void getPersonsByHobby() {
+    }
+
+    @Test
+    void getTotalNumberOfPersonsByHobby() {
+    }
+}

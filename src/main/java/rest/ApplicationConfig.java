df7f8c1ff98087ad1dbd106144fe673348498647
malenec <90346924+malenec@users.noreply.github.com>
2023-03-16 14:18:09 +0100

    new attempt with cors resource

--- src/main/java/rest/ApplicationConfig.java
+++ src/main/java/rest/ApplicationConfig.java
@@ -20,13 +20,13 @@ public class ApplicationConfig extends Application {
      * If required, comment out calling this method in getClasses().
      */
     private void addRestResourceClasses(Set<Class<?>> resources) {
-        resources.add(cors.CorsFilter.class);
         resources.add(errorhandling.GenericExceptionMapper.class);
         resources.add(org.glassfish.jersey.server.wadl.internal.WadlResource.class);
         resources.add(rest.RenameMeResource.class);
         resources.add(rest.PersonResource.class);
         resources.add(rest.HobbyResource.class);
         resources.add(rest.ZipCodeResource.class);
+        resources.add(cors.CorsFilter.class);
     }
     
 }
